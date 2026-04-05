55c69ff62977a182c0d3b24e43dab2f80c4769f3
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-04-05 16:48:04 +0500

    Update decimal.js

--- decimal/decimal.js
+++ decimal/decimal.js
@@ -192,11 +192,10 @@ class Decimal {
     }
   }
   toFixed(fixed=3) {
-    let k = (this.sign*this.man*10**this.exp).toString().split(".")
-    let m = (this.sign*this.man).toString().split(".")
-    [k[1],m[1]] ??= Array(2).fill("0".repeat(fixed))
-    k[1] = Math.floor(Math.abs((this.sign*this.man*10**(this.exp+fixed))%10**fixed))/10**fixed
-    m[1] = Math.floor(Math.abs((this.sign*this.man*10**fixed)%10**fixed))/10**fixed
+    var k = (this.sign*this.man*10**this.exp).toString().split(".")
+    var m = (this.sign*this.man).toString().split(".")
+    k[1] = Math.floor(Math.abs((this.sign*this.man*10**(this.exp+fixed))%10**fixed)).toString().padStart(fixed,0)
+    m[1] = Math.floor(Math.abs((this.sign*this.man*10**fixed)%10**fixed)).toString().padStart(fixed,0)
     if(this.exp===Infinity)return "inf"
     if (Math.abs(this.exp)>=1000000){
       return "e"+this.log10()
